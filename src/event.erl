eeaf62eeba4ab819a298eda9088851442c7eb1d7
neodevelop <neodevelop@gmail.com>
2018-03-11 15:10:26 -0600

    Many things:
    - Creating a record
    - Normalizing the time when is more than 50 days
        - Converting in a list
        - Consuming the head of the list for expiration

--- src/event.erl
+++ src/event.erl
@@ -1,10 +1,21 @@
 -module(event).
--export([loop/1]).
+-export([loop/1, normalize/1]).
+-record(state, {server,
+                name="",
+                to_go=0}).
 
-loop(State) ->
+loop(S = #state{server=Server, to_go=[T|Next]}) ->
   receive
     {Server, Ref, cancel} ->
-      % ...
-  after Delay
-        % ...
+      Server ! {Ref, ok}
+  after T*1000 ->
+    if Next =:= [] ->
+         Server ! {done, S#state.name};
+       Next =/= [] ->
+         loop(S#state{to_go=Next})
+    end
   end.
+
+normalize(N) ->
+  Limit = 49*24*60*60,
+  [N rem Limit | lists:duplicate(N div Limit, Limit)].
