ffb739abb383b7076281ebc031a6c51b6920cfa6
neodevelop <juan@makingdevs.com>
2018-03-12 13:13:36 -0600

    Uploading some modules for reference

---
+++ src/multiproc.erl
@@ -0,0 +1,38 @@
+-module(multiproc).
+-compile(export_all).
+
+sleep(T) ->
+  receive
+  after T -> ok
+  end.
+
+flush() ->
+  receive
+    _ -> flush()
+  after 0 ->
+    ok
+  end.
+
+important() ->
+ receive
+   {Priority, Message} when Priority > 10 ->
+     [Message | important()]
+ after 0 ->
+   normal()
+ end.
+
+normal() ->
+  receive
+    {_, Message} ->
+      [Message | normal()]
+  after 0 ->
+    []
+  end.
+
+optimized(Pid) ->
+  Ref = make_ref(),
+  Pid ! {self(), Ref, hello},
+  receive
+    {Pid, Ref, Msg} ->
+      io:format("~p~n", [Msg])
+  end.
