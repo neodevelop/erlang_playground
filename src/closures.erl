2bc764070fe6b40efae44d259055766719949683
neodevelop <neodevelop@gmail.com>
2018-03-18 15:11:58 -0600

    Making some closures

---
+++ src/closures.erl
@@ -0,0 +1,10 @@
+-module(closures).
+-compile([export_all]).
+
+multiplicador(X)  when is_integer(X) ->
+  fun(Y) -> X * Y end.
+
+enteros(Desde) ->
+  fun() ->
+    [Desde | enteros(Desde+1)]
+  end.
