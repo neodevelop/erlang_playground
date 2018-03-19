615f17f3379ab6046a6510bf27b9d70d31308503
neodevelop <neodevelop@gmail.com>
2018-03-19 14:19:48 -0600

    Renaming functions for mergesort

--- src/sorting.erl
+++ src/sorting.erl
@@ -4,15 +4,17 @@
 mergesort([]) -> [];
 mergesort([H]) -> [H];
 mergesort(L) ->
-  {L1,L2} = split_list(L),
-  mix(mergesort(L1), mergesort(L2)).
+  {L1,L2} = split_for_mergesort(L),
+  mix_for_mergesort(mergesort(L1), mergesort(L2)).
 
-split_list(L) ->
+split_for_mergesort(L) ->
   lists:split(length(L) div 2, L).
 
-mix([], L) -> L;
-mix(L, []) -> L;
-mix([H1|T1], [H2|_]=L2) when H1 =< H2 ->
-  [H1 | mix(T1, L2)];
-mix(L1, [H2|T2]) ->
-  [H2 | mix(L1, T2)].
+mix_for_mergesort([], L) -> L;
+mix_for_mergesort(L, []) -> L;
+mix_for_mergesort([H1|T1], [H2|_]=L2) when H1 =< H2 ->
+  [H1 | mix_for_mergesort(T1, L2)];
+mix_for_mergesort(L1, [H2|T2]) ->
+  [H2 | mix_for_mergesort(L1, T2)].
+
+
